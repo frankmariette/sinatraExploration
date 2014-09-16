9be7349efdb5c653c480e510b5104bb2476494bc
karrde00 <karrde00@gmail.com>
2014-09-15 23:39:37 -0500

    Adds foundation, bower and some basic stuff

--- server.rb
+++ server.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
 
 get '/' do
-	'Hello world'
+	erb :index, :layout => true
+end
+
+get '/rickroll' do
+	erb :rick, :layout => true
+end
+
+get '/posts/:id' do
+	"You are reading post #{params[:id]}"
 end
